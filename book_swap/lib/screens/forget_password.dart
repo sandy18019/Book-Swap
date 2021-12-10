4dcdf4cd7beb6bbd34d4a04b8a3f2ac18dfe596c
alyyasser7 <59626340+alyyasser7@users.noreply.github.com>
2021-12-10 15:29:12 +0200

    forget password

---
+++ book_swap/lib/screens/forget_password.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+
+class forgetpassword extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepOrange.shade100,
+      body: myforgetpassword(),
+    );
+  }
+}
+
+class myforgetpassword extends StatelessWidget{
+  const myforgetpassword({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+   return SingleChildScrollView(
+     
+   )
+  }
+
+}
