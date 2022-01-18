015cc01cf3c724c4dd414e7155ed425d8b1e756e
sandy18019 <76786766+sandy18019@users.noreply.github.com>
2022-01-18 20:11:09 +0200

    Update main.dart
    /

--- book_swap/lib/main.dart
+++ book_swap/lib/main.dart
@@ -16,7 +16,9 @@ import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized ();
   runApp(MyApp());
+  
 }
 
 class MyApp extends StatelessWidget {
